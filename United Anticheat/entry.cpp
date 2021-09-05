a918a86a1b5cf5946ba361d3f92cd6d16b3586ce
IAmMilxnorsPC <dsicordalt192@gmail.com>
2021-09-05 15:18:24 -0400

    gdfx

--- United Anticheat/entry.cpp
+++ United Anticheat/entry.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <Windows.h>
 
-int main()
+void CreateProcessAndWait(const char* path, const char* args, const char* dllPath = "0")
 {
+	// TODO: Add path checking to see if it exists/is a dll/is a exe.
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	if (!CreateProcessW(reinterpret_cast<LPCWSTR>(path),
+		reinterpret_cast<LPWSTR>(args),
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi)
+		)
+		std::cout << "Could not launch " + std::string(path); // TODO: Split at the last /
+	else
+	{
+		if(dllPath != "0") // Inject DLL
+		WaitForSingleObject(pi.hProcess, INFINITE);
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+}
+
+void main()
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
     std::cout << "Hello World!\n";
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	if (!CreateProcessW(L"ac",
+		reinterpret_cast<LPWSTR>("args"),
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi)
+		)
+		std::cout << "Cannot launch Fortnite.";
+	else
+	{
+		WaitForSingleObject(pi.hProcess, INFINITE);
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
 }
