405b2c2b30923449b38fb083e32d545aac5ccf4a
IAmMilxnorsPC <dsicordalt192@gmail.com>
2021-09-05 21:50:36 -0400

    i hate everytjing

--- United Anticheat/entry.cpp
+++ United Anticheat/entry.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #pragma warning (disable : 6276 ) // cast warning idk
+#pragma warning (disable : 4267 ) // loss of data warning (i should probably fix this but im to lazy)
 
 namespace fs = std::filesystem;
 
@@ -17,58 +18,89 @@ std::wstring widen(const std::string& s)
 	return std::wstring(&buf[0]);
 }
 
-void CreateProcessAndWait(std::string path, std::string args = " ", const char* dllPath = "0", bool fullDirectory = false) // yes i spent 6 hours on this function and i only call it twice. do i regret it, not at all.
+struct ProcessParams
+{
+	std::string exeName;
+	std::string exeArguments = " ";
+	const char* dllPath = "";
+	bool useCommandLine = false; // this may be useless i have to test.
+	DWORD creationFlags = CREATE_NEW_CONSOLE;
+	bool exeIsFullPath = false;
+	bool dllIsFullPath = false;
+};
+
+void NewProcess(const ProcessParams& params) // this took me too long.
 {
 	// TODO: Add path checking to see if it exists/is a dll/is a exe.
-	std::wstring wargs = widen(args);
-	if (wargs.size() != 0)
-	{
-		if (wargs[0] != L' ')
-		{
-			wargs.insert(0, L" ");
-		}
-	}
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	ZeroMemory(&pi, sizeof(pi));
+	
+	// Setting wpath and path to the full path
+
+	std::string path = params.exeName;
+	std::string args = params.exeArguments;
+	const char* dllPath = params.dllPath;
+	bool useCommandLine = params.useCommandLine;
+	DWORD creationFlags = params.creationFlags;
+	bool fullDirectory = params.exeIsFullPath;
+
+	if (path.find(".exe") == std::string::npos) path = path + ".exe"; // path.append(".exe");
+
 	std::wstring wpath;
 	fs::path fspath = fs::current_path() / path;
 	if (!fullDirectory)
 	{
 		path = fspath.string();
 		wpath = widen(path);
-		std::cout << fspath.string();
-		//fspath = path;
+		// std::cout << fspath.string() << std::endl;
 	}
 
-	wchar_t* wcp = new wchar_t[wargs.size() + 1];
-	if (wcp == 0)
+	LPCWSTR finalPath = NULL;
+	if (!useCommandLine)
 	{
-		// stuff
+		finalPath = const_cast<LPCWSTR>(wpath.c_str());
+		// creationFlags = CREATE_NO_WINDOW; // TODO: Test this lol
 	}
+	else args = path + " " + args; // args.insert(0, path);
+
+	
+	// Make sure arguments isn't nothing.
+	
+	std::wstring wargs = widen(args);
+	if (wargs.size() != 0)
+	{
+		if (wargs[0] != L' ') wargs.insert(0, L" ");
+	}
+
+	// memory stuff
+
+	wchar_t* wcp = new wchar_t[wargs.size() + 1];
 	const wchar_t* temp = wargs.c_str();
 	wcscpy_s(wcp, wargs.size() + 1, temp);
 
-	if (!CreateProcessW(const_cast<LPCWSTR>(wpath.c_str()),
-		wcp, // to print it out just cast it to a const char*
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	if (!CreateProcessW(finalPath,
+		wcp,
 		NULL,
 		NULL,
 		FALSE,
-		CREATE_NEW_CONSOLE,
+		creationFlags,
 		NULL,
 		NULL,
 		&si,
 		&pi)
 		)
 	{
-		std::cout << "Could not launch " + fspath.filename().string() + " Error: " << GetLastError() << std::endl;
+		std::cout << "Couldn't create process " + fspath.filename().string() + " and the error code is " << GetLastError() << std::endl;
 	}
 	else
 	{
-		if (dllPath != "0"); // Inject DLL
+		// if (fs::exists(dllPath)) // Inject DLL
 		WaitForSingleObject(pi.hProcess, INFINITE);
+		// std::cout << GetExitCodeProcess(pi.hProcess, idk what to put here) << std::endl;
 		delete[]wcp; // we may be able to do this earlier but i havent tested.
 		wcp = 0;
 		CloseHandle(pi.hProcess);
@@ -76,9 +108,11 @@ void CreateProcessAndWait(std::string path, std::string args = " ", const char* 
 	}
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	CreateProcessAndWait("LPWSTR Testing.exe");
-	CreateProcess
+	ProcessParams params;
+	params.exeName = "LPWSTR Testing.exe";
+	params.exeArguments = "Testing";
+	NewProcess(params);
 	std::cin.get();
 }
