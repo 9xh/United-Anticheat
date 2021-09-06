78a4ba1656a1ccc6ceda1179427d07efe1d7b244
IAmMilxnorsPC <dsicordalt192@gmail.com>
2021-09-06 09:55:23 -0400

    works pogger

--- United Anticheat/entry.cpp
+++ United Anticheat/entry.cpp
@@ -1,13 +1,30 @@
-#include <iostream>
-#include <Windows.h>
+// This whole thing took me way longer than it should have.
+
+// defines
+
+// #define LOGARGS
+
+// c++ includes
+
 #include <filesystem>
-#include <tchar.h>
-#include <stdio.h>
+#ifdef LOGARGS
+#include <fstream>
+#endif
 
 #pragma warning (disable : 6276 ) // cast warning idk
 #pragma warning (disable : 4267 ) // loss of data warning (i should probably fix this but im to lazy)
 
+#include "injector.h"
+
 namespace fs = std::filesystem;
+
+void Exit(const char* errorMsg, int exitCode) // TODO: Add support for closing (child) other applications.
+{
+	std::cout << errorMsg << std::endl;
+	std::cout << "Exiting in 3 seconds..." << std::endl;
+	Sleep(3000);
+	exit(exitCode);
+}
 
 std::wstring widen(const std::string& s)
 {
@@ -22,14 +39,13 @@ struct ProcessParams
 {
 	std::string exeName;
 	std::string exeArguments = " ";
-	const char* dllPath = "";
-	bool useCommandLine = false; // this may be useless i have to test.
+	const char* dllName = "";
 	DWORD creationFlags = CREATE_NEW_CONSOLE;
 	bool exeIsFullPath = false;
 	bool dllIsFullPath = false;
 };
 
-void NewProcess(const ProcessParams& params) // this took me too long.
+void NewProcess(const ProcessParams& params)
 {
 	// TODO: Add path checking to see if it exists/is a dll/is a exe.
 	
@@ -37,30 +53,20 @@ void NewProcess(const ProcessParams& params) // this took me too long.
 
 	std::string path = params.exeName;
 	std::string args = params.exeArguments;
-	const char* dllPath = params.dllPath;
-	bool useCommandLine = params.useCommandLine;
+	const char* dllPath = params.dllName;
 	DWORD creationFlags = params.creationFlags;
 	bool fullDirectory = params.exeIsFullPath;
 
-	if (path.find(".exe") == std::string::npos) path = path + ".exe"; // path.append(".exe");
+	if (path.find(".exe") == std::string::npos) path = path + ".exe"; // TODO: Test path.append(".exe");
 
 	std::wstring wpath;
 	fs::path fspath = fs::current_path() / path;
 	if (!fullDirectory)
 	{
-		path = fspath.string();
-		wpath = widen(path);
+		// path = fspath.string(); // not sure if we need to update this as we may be able to just pass fspath.string() directly into wpath.
+		wpath = widen(fspath.string());
 		// std::cout << fspath.string() << std::endl;
 	}
-
-	LPCWSTR finalPath = NULL;
-	if (!useCommandLine)
-	{
-		finalPath = const_cast<LPCWSTR>(wpath.c_str());
-		// creationFlags = CREATE_NO_WINDOW; // TODO: Test this lol
-	}
-	else args = path + " " + args; // args.insert(0, path);
-
 	
 	// Make sure arguments isn't nothing.
 	
@@ -82,7 +88,9 @@ void NewProcess(const ProcessParams& params) // this took me too long.
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
 
-	if (!CreateProcessW(finalPath,
+	std::cout << "Starting " << params.exeName << std::endl;
+
+	if (!CreateProcessW(const_cast<LPCWSTR>(wpath.c_str()),
 		wcp,
 		NULL,
 		NULL,
@@ -98,21 +106,89 @@ void NewProcess(const ProcessParams& params) // this took me too long.
 	}
 	else
 	{
-		// if (fs::exists(dllPath)) // Inject DLL
+
+		if (dllPath != "")
+		{
+			std::string fullDllPath = (fs::current_path() / dllPath).string();
+			std::cout << InjectDLL(pi.dwProcessId, fullDllPath, dllPath ? "Injected DLL successfully!" : "Couldn't inject DLL.") << std::endl;
+		}
 		WaitForSingleObject(pi.hProcess, INFINITE);
 		// std::cout << GetExitCodeProcess(pi.hProcess, idk what to put here) << std::endl;
 		delete[]wcp; // we may be able to do this earlier but i havent tested.
 		wcp = 0;
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
+
+		// Getting whole path and parsing it to only the filename.
+
+		char path[MAX_PATH] = ""; // __FILE__ could also be a solution, but I'm pretty sure its on compile time.
+		std::string strAppName;
+
+		GetModuleFileNameA(0, path, MAX_PATH);
+
+		// Extract name
+		strAppName = path;
+		strAppName = strAppName.substr(strAppName.rfind("\\") + 1);
+
+		// Starting BE/EAC
+
+		if (strAppName.find("BE") != std::string::npos) system("net stop BEService"); // we could check in args what kind of anticheat it is but imo this is better.
+		else if (strAppName.find("EAC") != std::string::npos) system("net stop EasyAnticheat");
+		else std::cout << "Major issue. Please send this to Milxnor: " << (fs::current_path() / strAppName).string() << std::endl;
+
+		Sleep(1000);
+		exit(0);
 	}
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
+
+	// Getting whole path and parsing it to only the filename.
+
+	char path[MAX_PATH] = ""; // __FILE__ could also be a solution, but I'm pretty sure its on compile time.
+	std::string strAppName;
+
+	GetModuleFileNameA(0, path, MAX_PATH);
+
+	// Extract name
+	strAppName = path;
+	strAppName = strAppName.substr(strAppName.rfind("\\") + 1);
+
+	// Starting BE/EAC
+
+	if (strAppName.find("BE") != std::string::npos) system("net start BEService"); // we could check in args what kind of anticheat it is but imo this is better.
+	else if (strAppName.find("EAC") != std::string::npos) system("net start EasyAnticheat");
+	else std::cout << "Major issue. Please send this to Milxnor: " << (fs::current_path() / strAppName).string() << std::endl;
+
+	// Converting the args to a string
+
+	std::string argumentsStr = "";
+	if (argc > 0)
+	{
+#ifdef LOGARGS
+		std::ofstream outfile("args.txt");
+#endif
+		for (int i = 0; i < argc; ++i) {
+#ifdef LOGARGS
+			outfile << argv[i] << " ";
+#endif
+			argumentsStr.append(std::string(argv[i]).append(" "));
+		}
+#ifdef LOGARGS 
+		outfile.close(); 
+#endif
+	}
+
+	// Starting main shipping process.
+
 	ProcessParams params;
-	params.exeName = "LPWSTR Testing.exe";
-	params.exeArguments = "Testing";
+
+	params.exeName = "FortniteClient-Win64-Shipping.exe";
+	params.exeArguments = argumentsStr;
+	params.dllName = "United.dll";
+
 	NewProcess(params);
+
 	std::cin.get();
 }
